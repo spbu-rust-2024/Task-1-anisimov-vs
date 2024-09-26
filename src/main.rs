2cf1bf9750c05d599e94f0a6520938edfb0999f9
Anisimov Vasiliy <st129629@student.spbu.ru>
2024-09-26 19:50:47 +0300

    create task 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
+use std::io;
 
+fn shell_sort(array: &mut Vec<i32>) {
+    let len_array: usize = array.len();
+    let mut gap: usize = len_array / 2;
+    while gap > 0 {
+        for i in gap..len_array {
+            let temp: i32 = array[i];
+            let mut j: usize = i;
+            while (j >= gap) && (array[j - gap] > temp) {
+                array[j] = array[j - gap];
+                j -= gap;
+            }
+            array[j] = temp;
+        }
+        gap /= 2;
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let mut array: Vec<i32> = input
+        .split_whitespace()
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
+    shell_sort(&mut array);
+    let output = array
+        .iter()
+        .map(|&num| num.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+    println!("{}", output);
 }
