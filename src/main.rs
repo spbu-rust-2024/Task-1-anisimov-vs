941c0ddc628b20c08c86b828db7e33ce10d0a689
Anisimov Vasiliy <st129629@student.spbu.ru>
2024-09-27 01:07:53 +0300

    change from i32 to i16 and make the code a little more readable

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,49 @@
 use std::io;
 
-fn shell_sort(array: &mut Vec<i32>) {
+fn shell_sort(array: &mut Vec<i16>) {
     let len_array: usize = array.len();
     let mut gap: usize = len_array / 2;
+
     while gap > 0 {
         for i in gap..len_array {
-            let temp: i32 = array[i];
+            let temp: i16 = array[i];
             let mut j: usize = i;
+
             while (j >= gap) && (array[j - gap] > temp) {
                 array[j] = array[j - gap];
                 j -= gap;
             }
+
             array[j] = temp;
         }
+
         gap /= 2;
     }
 }
 
-fn main() {
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let mut array: Vec<i32> = input
-        .split_whitespace()
-        .map(|s| s.parse().expect("parse error"))
-        .collect();
-    shell_sort(&mut array);
-    let output = array
+fn print_array(vector: &Vec<i16>) {
+    let output = vector
         .iter()
         .map(|&num| num.to_string())
         .collect::<Vec<String>>()
         .join(" ");
-    println!("{}", output);
+
+    println!("{output}");
+}
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let mut array: Vec<i16> = input
+        .split_whitespace()
+        .map(|s| s.parse().expect("parse error"))
+        .collect();
+
+    shell_sort(&mut array);
+
+    print_array(&array);
 }
